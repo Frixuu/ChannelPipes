45157408ab95521b7d53a8015b87e2129642239f
Frixuu <kontakt@lukasz.xyz>
2022-02-08 01:39:28 +0100

    Stub features

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,18 @@
 #[cfg(feature = "crossbeam")]
 pub mod crossbeam;
 
+#[cfg(feature = "mpsc")]
+compile_error!("channel_pipes doesn't work with std::sync::mpsc yet");
+
+#[cfg(feature = "bus")]
+compile_error!("channel_pipes doesn't work with the bus crate yet");
+
+#[cfg(feature = "flume")]
+compile_error!("channel_pipes doesn't work with the flume crate yet");
+
+#[cfg(feature = "futures")]
+compile_error!("channel_pipes doesn't work with futures-channel yet");
+
 pub trait PipelineStage<T> {
     fn select(&self, el: T) -> Option<T>;
 }
