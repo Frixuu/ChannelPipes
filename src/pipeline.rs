0d275054c455c81a11501a9a9020c7a0d8795da6
Frixuu <kontakt@lukasz.xyz>
2022-02-08 22:10:09 +0100

    Provide concrete implementations of PipelineStage

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,6 +1,43 @@
+use std::marker::PhantomData;
+
 /// Describes a stage of mapping, filtering etc. of elements passed through a channel.
 pub trait PipelineStage<T, R> {
-    /// If an element gets promoted to the next stage, returns that element, optionally mapped.
+    /// If an element gets promoted to the next stage,
+    /// returns a result of a mapping function on that element.
     /// If that element does not pass the stage, returns None.
     fn apply(&self, element: T) -> Option<R>;
+}
+
+/// Folds two different pipeline stages.
+struct CompositePipelineStage<I, B, R> {
+    left: Box<dyn PipelineStage<I, B>>,
+    right: Box<dyn PipelineStage<B, R>>,
+}
+
+impl<I, B, R> PipelineStage<I, R> for CompositePipelineStage<I, B, R> {
+    fn apply(&self, element: I) -> Option<R> {
+        self.left
+            .apply(element)
+            .map(|e| self.right.apply(e))
+            .flatten()
+    }
+}
+
+// A pipeline stage that accepts every element.
+struct IdentityPipelineStage<T> {
+    _marker: PhantomData<T>,
+}
+
+impl<T> IdentityPipelineStage<T> {
+    fn new() -> Self {
+        IdentityPipelineStage {
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> PipelineStage<T, T> for IdentityPipelineStage<T> {
+    fn apply(&self, element: T) -> Option<T> {
+        Some(element)
+    }
 }
